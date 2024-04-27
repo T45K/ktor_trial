807268f4d1b6595034ffb05dfb25a2565075354b
T45K <tasktas9@gmail.com>
2024-04-27 17:16:20 +0900

    generate SDL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,34 @@
+import com.expediagroup.graphql.plugin.gradle.tasks.GraphQLGenerateSDLTask
+
 plugins {
     id("io.ktor.plugin") version "2.3.10"
     kotlin("jvm") version "2.0.0-RC1"
+
+    id("com.expediagroup.graphql") version "7.1.1"
 }
 
 repositories {
     mavenCentral()
 }
 
+val graphqlKotlinVersion = "7.1.1"
+
 dependencies {
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-netty")
+
+    implementation("com.expediagroup:graphql-kotlin-ktor-server:$graphqlKotlinVersion")
 
     implementation("ch.qos.logback:logback-classic:1.5.6")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation(libs.junit.jupiter.engine)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+val graphqlGenerateSDL by tasks.getting(GraphQLGenerateSDLTask::class) {
+    packages.set(listOf("io.github.t45k.ktor_trial"))
+    schemaFile.set(file("${project.projectDir}/schema.graphql"))
 }
 
 java {
--- app/src/main/kotlin/io/github/t45k/ktor_trial/main.kt
+++ app/src/main/kotlin/io/github/t45k/ktor_trial/main.kt
@@ -1,9 +1,15 @@
 package io.github.t45k.ktor_trial
 
+import com.expediagroup.graphql.server.ktor.GraphQL
+import com.expediagroup.graphql.server.ktor.graphQLPostRoute
+import com.expediagroup.graphql.server.operations.Query
+import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.response.respondText
+import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
@@ -13,4 +19,22 @@ fun main() {
             get("/") { call.respondText("Hello World!") }
         }
     }.start(wait = true)
+}
+
+class HelloWorldQuery : Query {
+    fun hello(): String = "Hello World!"
+}
+
+fun Application.graphQLModule() {
+    install(GraphQL) {
+        schema {
+            packages = listOf("io.github.t45k.ktor_trial")
+            queries = listOf(
+                HelloWorldQuery(),
+            )
+        }
+    }
+    install(Routing) {
+        graphQLPostRoute()
+    }
 }
